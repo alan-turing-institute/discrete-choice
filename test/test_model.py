079a60c3d925244d96571ff731f736f69c35dff5
Jim Madge <jmmadge@gmail.com>
2019-03-05 14:39:22 +0000

    Add trivial tests

--- test/test_model.py
+++ test/test_model.py
@@ -1,5 +1,6 @@
 from .context import add_project_path, data_dir
 import choice_model
+import pandas
 import pytest
 
 add_project_path()
@@ -15,5 +16,28 @@ def simple_model():
 class TestChoiceModel():
     def test_model_title(self, simple_model):
         model = simple_model
-
         assert model.title == 'Simple model'
+
+    def test_model_data(self, simple_model):
+        model = simple_model
+        assert all(model.data == pandas.read_csv(data_dir+'simple.csv'))
+
+    def test_model_choices(self, simple_model):
+        model = simple_model
+        assert model.choices == ['choice1', 'choice2']
+
+    def test_model_choice_column(self, simple_model):
+        model = simple_model
+        assert model.choice_column == 'alternative'
+
+    def test_model_variables(self, simple_model):
+        model = simple_model
+        assert model.variables == ['var1', 'var2', 'var3', 'var4']
+
+    def test_model_intercepts(self, simple_model):
+        model = simple_model
+        assert model.intercepts == {'var1': 'cchoice1'}
+
+    def test_model_parameters(self, simple_model):
+        model = simple_model
+        assert model.parameters == ['p1', 'p2']
