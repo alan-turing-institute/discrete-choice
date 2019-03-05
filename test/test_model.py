dfa0b8a5f5494d314cb5158c560fa2e01a5b1fae
Jim Madge <jmmadge@gmail.com>
2019-03-05 15:09:58 +0000

    Add tests for raising exceptions

--- test/test_model.py
+++ test/test_model.py
@@ -49,3 +49,29 @@ class TestChoiceModel():
     def test_model_parameters(self, simple_model):
         model = simple_model
         assert model.parameters == ['p1', 'p2']
+
+
+def test_missing_yaml_key():
+    with pytest.raises(choice_model.MissingYamlKey):
+        with open(data_dir+'missing_title.yml', 'r') as yaml_file:
+            choice_model.ChoiceModel.from_yaml(yaml_file)
+
+
+def test_undefined_availability():
+    with pytest.raises(choice_model.UndefinedAvailability):
+        with open(data_dir+'undefined_availability.yml', 'r') as yaml_file:
+            choice_model.ChoiceModel.from_yaml(yaml_file)
+
+
+def test_incorrect_intercepts():
+    with pytest.raises(choice_model.IncorrectNumberOfIntercepts):
+        with open(data_dir+'incorrect_intercepts.yml', 'r') as yaml_file:
+            choice_model.ChoiceModel.from_yaml(yaml_file)
+
+
+def test_missing_field():
+    with pytest.raises(choice_model.MissingField):
+        with open(data_dir+'simple_model.yml', 'r') as yaml_file,\
+                open(data_dir+'missing_field.csv', 'r') as data_file:
+            model = choice_model.ChoiceModel.from_yaml(yaml_file)
+            model.load_data(data_file)
