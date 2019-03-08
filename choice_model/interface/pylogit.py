a0adfb78bb2f4657fddf9678b29790775d9cd6cc
Jim Madge <jmmadge@gmail.com>
2019-03-08 10:19:42 +0000

    Add conversion to pylogit long data format

--- choice_model/interface/pylogit.py
+++ choice_model/interface/pylogit.py
@@ -4,6 +4,8 @@ pylogit interface
 
 from . import Interface
 from .. import MultinomialLogit
+import numpy as np
+import pylogit as pl
 
 
 class PylogitInterface(Interface):
@@ -11,3 +13,59 @@ class PylogitInterface(Interface):
 
     def __init__(self, model):
         super().__init__(model)
+
+    def _encode_choices_as_integers(self):
+        """
+        Convert choice labels from strings to integers as pylogit expects
+        """
+        model = self.model
+        number_of_choices = model.number_of_choices()
+        # Create mapping from choice strings to integers begining from 0
+        choice_encoding = dict(zip(model.choices, range(number_of_choices)))
+
+        # Create alternative specific variables dictionary using the integer
+        # encoding
+        alt_specific_vars = {}
+        for variable in model.choice_dependent_variables.keys():
+            temp = model.choice_dependent_variables[variable]
+            # Replace string choice key with integer key
+            temp = {choice_encoding[key]: value for key, value in temp.items()}
+            alt_specific_vars[variable] = temp
+
+        # Create availaibility using the integer encoding
+        availability_vars = {choice_encoding[key]: value
+                             for key, value in model.availability.items()}
+
+        # Create a dataframe column with the integer encoding. In the long
+        # format this column will be a boolean for whether this choice was made
+        # or not.
+        model.data['choice_bool'] = model.data[model.choice_column].apply(
+            lambda x: choice_encoding[x])
+
+        return alt_specific_vars, availability_vars
+
+    def _convert_to_long_format(self):
+        """
+        Convert data to the long format expected by pylogit
+        """
+        model = self.model
+        (alt_specific_vars,
+         availability_vars) = self._encode_choices_as_integers()
+        # Create observation number column as a range of integers from 1
+        self.model.data['observation_id'] = np.arange(model.data.shape[0],
+                                                      dtype=int)+1
+
+        # Use pylogit routine to convert to long format
+        self.long_data = pl.convert_wide_to_long(
+            wide_data=model.data,
+            ind_vars=model.choice_independent_variables,
+            alt_specific_vars=alt_specific_vars,
+            availability_vars=availability_vars,
+            obs_id_col='observation_id',
+            choice_col='choice_bool',
+            new_alt_id_name='choice_id'
+            )
+
+        # Remove choice bool and observation_id column from wide data
+        model.data.drop(columns=['choice_bool', 'observation_id'],
+                        inplace=True)
--- test/test_interface_pylogit.py
+++ test/test_interface_pylogit.py
@@ -5,16 +5,30 @@ import pytest
 add_project_path()
 
 
-@pytest.fixture(scope='class')
+@pytest.fixture(scope='module')
 def simple_multinomial_model():
     with open(data_dir+'simple_model.yml', 'r') as yaml_file:
         return choice_model.MultinomialLogit.from_yaml(yaml_file)
 
 
-@pytest.fixture(scope="class")
+@pytest.fixture(scope="module")
 def simple_model():
     with open(data_dir+'simple_model.yml', 'r') as yaml_file:
         return choice_model.ChoiceModel.from_yaml(yaml_file)
+
+
+@pytest.fixture(scope="module")
+def simple_multinomial_model_with_data():
+    with open(data_dir+'simple_model.yml', 'r') as yaml_file,\
+            open(data_dir+'simple.csv', 'r') as data_file:
+        model = choice_model.MultinomialLogit.from_yaml(yaml_file)
+        model.load_data(data_file)
+        return model
+
+
+@pytest.fixture(scope="module")
+def simple_multinomial_pylogit_interface(simple_multinomial_model_with_data):
+    return choice_model.PylogitInterface(simple_multinomial_model_with_data)
 
 
 class TestPylogitInterface():
@@ -25,3 +39,47 @@ class TestPylogitInterface():
     def test_simple_model(self, simple_model):
         with pytest.raises(TypeError):
             choice_model.PylogitInterface(simple_model)
+
+
+class TestPylogitLongData():
+    def test_long_data(self, simple_multinomial_pylogit_interface):
+        interface = simple_multinomial_pylogit_interface
+        interface._convert_to_long_format()
+
+    @pytest.mark.parametrize(
+        'wide_data_key,long_data_key,observation,choice', [
+            ('var1', 'var1', 0, 0),
+            ('var1', 'var1', 0, 1),
+            ('var2', 'var2', 0, 0),
+            ('var2', 'var2', 0, 1),
+            ('var1', 'var1', 1, 0),
+            ('var1', 'var1', 1, 1),
+            ('var2', 'var2', 1, 0),
+            ('var2', 'var2', 1, 1),
+            ('choice1_var3', 'var3', 0, 0),
+            ('choice2_var3', 'var3', 0, 1),
+            ('choice1_var3', 'var3', 1, 0),
+            ('choice2_var3', 'var3', 1, 1)
+            ]
+        )
+    def test_variables(self, simple_multinomial_pylogit_interface,
+                       wide_data_key, long_data_key, observation,
+                       choice):
+        interface = simple_multinomial_pylogit_interface
+        wide_data = interface.model.data
+        long_data = interface.long_data
+        wide_value = wide_data[wide_data_key][observation]
+        long_value = long_data[long_data_key][observation*2+choice]
+        assert wide_value == long_value
+
+    @pytest.mark.parametrize('observation', [0, 1])
+    def test_choices(self, simple_multinomial_pylogit_interface, observation):
+        interface = simple_multinomial_pylogit_interface
+        wide_data = interface.model.data
+        long_data = interface.long_data
+        if wide_data['alternative'][observation] == 'choice1':
+            assert long_data['choice_bool'][observation*2] == 1
+        elif wide_data['alternative'][observation] == 'choice2':
+            assert long_data['choice_bool'][observation*2+1] == 1
+        else:
+            assert 0
