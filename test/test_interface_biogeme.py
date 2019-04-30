0a2f05b0a8c6a8cf945a7f44cda18c42bb18acf1
Jim Madge <jmmadge@gmail.com>
2019-04-30 16:11:59 +0100

    Add Grenoble model tests for Biogeme

--- test/test_interface_biogeme.py
+++ test/test_interface_biogeme.py
@@ -22,6 +22,42 @@ class TestBiogemeInterface():
 # Tests for constructing utilities
 
 
+@pytest.fixture(scope='module')
+def grenoble_estimation(main_data_dir):
+    with open(main_data_dir+'grenoble.yml') as model_file,\
+            open(main_data_dir+'grenoble.csv') as data_file:
+        model = choice_model.MultinomialLogit.from_yaml(model_file)
+        model.load_data(data_file)
+    interface = choice_model.BiogemeInterface(model)
+    interface.estimate()
+    return interface
+
+
+class TestBiogemeGrenobleEstimation():
+    @pytest.mark.parametrize('parameter,value', [
+        ('cpt', 1.098191),
+        ('ccycle', 0.597608),
+        ('cwalk', 2.099543),
+        ('cpass', -2.730642),
+        ('phead_of_household', -0.830965),
+        ('porigin_walk', -0.001890),
+        ('pcar_competition', 2.654556),
+        ('phas_car', 1.122745),
+        ('pfemale_passenger', 0.848129),
+        ('pfemale_cycle', -0.919043),
+        ('pcentral_zone', -1.481088),
+        ('pmanual_worker', 0.755348),
+        ('ptime', -0.000384),
+        ('pcost', -0.001127),
+        ('pnon_linear', -0.003240)
+        ])
+    def test_optimised_parameters(self, grenoble_estimation,
+                                  parameter, value):
+        interface = grenoble_estimation
+        parameters = interface.parameters()
+        assert parameters[parameter] == pytest.approx(value, rel=2.0e-3)
+
+
 class TestPylogitRequiresEstimation():
     @pytest.mark.parametrize('method', [
         'display_results',
